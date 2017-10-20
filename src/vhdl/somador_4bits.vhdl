b67c8a379645e39230ce92c4dce86c867e4ba6ec
Victor Franco <victorfrancovl@gmail.com>
2017-10-20 12:01:42 -0300

    Correção somador 4bits

--- src/vhdl/somador_4bits.vhdl
+++ src/vhdl/somador_4bits.vhdl
@@ -24,10 +24,10 @@ architecture structural of somador_4bits is
 			vss       : in  std_logic
 		);
 	end component;
-	signal c1, c2, c3, c4: std_logic;
+	signal c1, c2, c3: std_logic;
 begin
 	S0: somador_1bit port map (a(0), b(0), cin, s(0), c1);
 	S1: somador_1bit port map (a(1), b(1), c1,  s(1), c2);
-	S2: somador_1bit port map (a(2), b(2), c3,  s(2), c4);
-	S3: somador_1bit port map (a(3), b(3), c4,  s(3), cout);
+	S2: somador_1bit port map (a(2), b(2), c2,  s(2), c3);
+	S3: somador_1bit port map (a(3), b(3), c3,  s(3), cout);
 end structural;
